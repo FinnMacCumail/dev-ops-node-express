aaa7d5daba3b85599af6c29bd1ef372d8c773215
FinnMacCumail <derm.finn@gmail.com>
2022-06-11 10:44:01 +0000

    sec com

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGO_IP: process.env.MONGO_IP || "mongo",
+    MONO_PORT: process.env.MONO_PORT || 27012,
+    MONGO_USER: process.env.MONGO_USER,
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD
+}
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const momgoose = require("mongoose");
+const {MONGO_USER, MONGO_PASSWORD, MONGO_IP , MONO_PORT} = require("./config/config");
 
 const app = express();
 
 mongoose
-    .connect("mongodb://derm:vidal*12@mongo:27017/?authSource=admin")
+    .connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONO_PORT}/?authSource=admin`)
     .then(() => console.log("successfully connected to mongo database"))
     .catch((e) => console.log(e));
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h2>Hi There</h2>");
+    res.send("<h2>Hi There!!!</h2>");
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`))
